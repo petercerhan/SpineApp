9826b64f3b09436e8ee4d87e8cd3613be0defcc1
petercerhan <petercerhan@gmail.com>
2017-05-27 20:07:32 -0500

    Create predictor model structure

---
+++ SpineApp/Predictor.swift
@@ -0,0 +1,75 @@
+//
+//  Predictor.swift
+//  SpineApp
+//
+//  Created by Peter Cerhan on 5/27/17.
+//  Copyright © 2017 Peter Cerhan. All rights reserved.
+//
+
+import Foundation
+
+enum PredictorCode {
+    case predictor1, predictor2, predictor3, predictor4, predictor5, predictor6, predictor7, predictor8, predictor9, predictor10
+}
+
+struct Predictor {
+    var type: PredictorCode
+    var name: String?
+    var description: String?
+    
+    init(type: PredictorCode) {
+        self.type = type
+    }
+
+    mutating func setName() {
+        switch type {
+        case .predictor1:
+            name = "Predictor 1"
+        case .predictor2:
+            name = "Predictor 2"
+        case .predictor3:
+            name = "Predictor 3"
+        case .predictor4:
+            name = "Predictor 4"
+        case .predictor5:
+            name = "Predictor 5"
+        case .predictor6:
+            name = "Predictor 6"
+        case .predictor7:
+            name = "Predictor 7"
+        case .predictor8:
+            name = "Predictor 8"
+        case .predictor9:
+            name = "Predictor 9"
+        case .predictor10:
+            name = "Predictor 10"
+        }
+    }
+    
+    mutating func setDescription() {
+        switch type {
+        case .predictor1:
+            description = "Description 1"
+        case .predictor2:
+            description = "Description 2"
+        case .predictor3:
+            description = "Description 3"
+        case .predictor4:
+            description = "Description 4"
+        case .predictor5:
+            description = "Description 5"
+        case .predictor6:
+            description = "Description 6"
+        case .predictor7:
+            description = "Description 7"
+        case .predictor8:
+            description = "Description 8"
+        case .predictor9:
+            description = "Description 9"
+        case .predictor10:
+            description = "Description 10"
+        }
+    }
+    
+
+}
