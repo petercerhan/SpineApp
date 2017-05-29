92247811a5f21e2c391e6b8201be788c67b7af83
petercerhan <petercerhan@gmail.com>
2017-05-29 16:25:18 -0500

    Add container view controller and set initial view controller to OpenScene view controller programmatically

---
+++ MainContainerViewController.swift
@@ -0,0 +1,85 @@
+//
+//  MainContainerViewController.swift
+//  SpineApp
+//
+//  Created by Peter Cerhan on 5/29/17.
+//  Copyright © 2017 Peter Cerhan. All rights reserved.
+//
+
+import UIKit
+
+class MainContainerViewController: UIViewController {
+    
+    private var contentViewController: UIViewController
+    let appStoryboard = UIStoryboard(name: "Main", bundle: nil)
+    
+    var hideStatusBar = true
+    override var prefersStatusBarHidden: Bool {
+        return hideStatusBar
+    }
+    
+    init() {
+        let vc = appStoryboard.instantiateViewController(withIdentifier: "OpenSceneViewController") as! OpenSceneViewController
+        contentViewController = vc
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        //Never instantiated from resource file
+        contentViewController = UIViewController()
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(contentViewController.view)
+    }
+    
+    func update(contentViewController newViewController: UIViewController) {
+        
+        let priorViewController = contentViewController
+        
+        contentViewController = newViewController
+        
+        addChildViewController(newViewController)
+        
+        newViewController.view.frame = view.bounds
+        view.addSubview(newViewController.view)
+        
+        priorViewController.willMove(toParentViewController: nil)
+        newViewController.didMove(toParentViewController: self)
+        
+        //animate here
+        
+        priorViewController.view.removeFromSuperview()
+        priorViewController.removeFromParentViewController()
+    }
+    
+    func updateAnimated(contentViewController newViewController: UIViewController) {
+        
+        let priorViewController = contentViewController
+        
+        contentViewController = newViewController
+        
+        addChildViewController(newViewController)
+        
+        newViewController.view.frame = view.bounds
+        newViewController.view.center.x += view.frame.width
+        view.addSubview(newViewController.view)
+        
+        priorViewController.willMove(toParentViewController: nil)
+        newViewController.didMove(toParentViewController: self)
+        
+        //animate here
+        UIView.animate(withDuration: 0.3, animations: {
+            _ in
+            newViewController.view.center.x -= self.view.frame.width
+            priorViewController.view.center.x -= self.view.frame.width
+        }, completion:{
+            _ in
+            priorViewController.view.removeFromSuperview()
+            priorViewController.removeFromParentViewController()
+        })
+        
+    }
+}
--- SpineApp/ViewController.swift
+++ SpineApp/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class OpenSceneViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
