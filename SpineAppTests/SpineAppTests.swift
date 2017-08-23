a8319b01ca3e6a0305c13a61a92d3feb5064a025
petercerhan <petercerhan@gmail.com>
2017-08-22 19:01:43 -0500

    Complete tests for outcomes presenter

--- SpineAppTests/SpineAppTests.swift
+++ SpineAppTests/SpineAppTests.swift
@@ -18,6 +18,7 @@ class SpineAppTests: XCTestCase {
         
         func set(elements: [OutcomesElement], evaluated: [Bool]) {
             self.elements = elements
+            self.evaluated = evaluated
         }
 
         var elementSet: OutcomesElement?
@@ -96,19 +97,24 @@ class SpineAppTests: XCTestCase {
     var presenterUnderTest: OutcomesPresenter!
     var mockView: MockOutcomesView!
     var mockCoordinator: MockOutcomesCoordinator!
+    var mockStateController: MockOutcomesStateController!
     
     override func setUp() {
         super.setUp()
         
         mockView = MockOutcomesView()
         mockCoordinator = MockOutcomesCoordinator()
+        mockStateController = MockOutcomesStateController()
         
-        presenterUnderTest = OutcomesPresenter(delegate: mockCoordinator, outcomesStateController: MockOutcomesStateController())
+        presenterUnderTest = OutcomesPresenter(delegate: mockCoordinator, outcomesStateController: mockStateController)
         presenterUnderTest.attach(view: mockView)
     }
     
     override func tearDown() {
         presenterUnderTest = nil
+        mockView = nil
+        mockCoordinator = nil
+        mockStateController = nil
         
         super.tearDown()
     }
@@ -118,10 +124,40 @@ class SpineAppTests: XCTestCase {
         XCTAssertNotNil(presenterUnderTest.view, "View not attached")
     }
     
+    func test_DataLoaded() {
+        XCTAssertEqual(mockView.elements.count, 0, "Mock View Data not properly initialized")
+        
+        presenterUnderTest.loadData()
+        
+        XCTAssertEqual(mockView.elements.count, 3, "Mock view data did not load")
+    }
+
+    func test_ResetAll() {
+        XCTAssertEqual(mockStateController.resetAllCalled, false, "Mock state controller not reset all not properly initialized")
+        XCTAssertEqual(mockView.elements.count, 0, "Mock View Data not properly initialized")
+        XCTAssertEqual(mockView.evaluated.count, 0, "Mock view data not properly initialized")
+        
+        presenterUnderTest.resetAll()
+        
+        XCTAssertEqual(mockStateController.resetAllCalled, true, "Reset all not called on state controller")
+        XCTAssertEqual(mockView.elements.count, 3, "Mock view elements not updated")
+        XCTAssertEqual(mockView.evaluated.count, 3, "Mock view evaluated not updated")
+    }
+
+    func test_NomogramSelected() {
+        XCTAssertEqual(mockStateController.nomogramEvaluated[1], false, "Mock state controller not properly initialized")
+        XCTAssertNil(mockCoordinator.nomogramSelectedIndex, "Mock coordinator not properly initialized")
+        
+        presenterUnderTest.nomogramSelected(atIndex: 1)
+        
+        XCTAssertEqual(mockStateController.nomogramEvaluated[1], true, "Nomogram evaulated not set in state controller")
+        XCTAssertEqual(mockCoordinator.nomogramSelectedIndex, 1, "Nomogramevaluated not called at proper index")
+    }
     
-    
-    
-    
+//    func nomogramSelected(atIndex index: Int) {
+//        outcomesStateController.setNomogramEvaluated(atIndex: index)
+//        delegate?.nomogramSelected(self, atIndex: index)
+//    }
     
 }
 
